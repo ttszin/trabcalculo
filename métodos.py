476fc6a37385aca6343dfebde2a0f782b55dfa3d
Matheus Teixeira de Souza <95655266+ttszin@users.noreply.github.com>
2023-05-30 19:35:52 -0300

    Update métodos.py

--- métodos.py
+++ métodos.py
@@ -12,15 +12,23 @@ def funcao():
     t = 300
     i2 = 2
     r = 330
-    i = 6
+    v = 6
+
+
+    i = v/r
+    print(i)
+
+    accuracy = 0.0001
 
     cal = ln((i/i2)+1) 
     
     vd = (k*t/q)*cal            # t = temperatura do ambiente em kelvin // q = carga elétrica do elétron // i2 = corrente de saturação do diodo // k = 1,38 x 10^-23  
+    vr = r*i                               # tensão no resistor = resistencia x tensão
     
-    vr = r*i                                # tensão no resistor = resistencia x tensão
     f = vr - vd - i*r                    # i = corrente no circuito // r = resistência no resistor // k = constante de Boltzmann // 
 
+    print(vd)
+    print(vr)
     print(f)
 
 
