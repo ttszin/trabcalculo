2883b8175f3b32489dfff50c7a0f30ba9a680dfa
Matheus Teixeira de Souza <95655266+ttszin@users.noreply.github.com>
2023-05-30 19:18:03 -0300

    Update métodos.py

--- métodos.py
+++ métodos.py
@@ -1,5 +1,6 @@
 #coding: UTF-8
 import math 
+from numpy import log as ln
 
 def main():
     funcao()
@@ -11,11 +12,16 @@ def funcao():
     t = 300
     i2 = 2
     r = 330
-    v = 6
+    i = 6
+
+    cal = ln((i/i2)+1) 
     
-    vd = k*t/q.log((i/i2)+1)             # t = temperatura do ambiente em kelvin // q = carga elétrica do elétron // i2 = corrente de saturação do diodo // k = 1,38 x 10^-23  
-    vr = r*i
+    vd = (k*t/q)*cal            # t = temperatura do ambiente em kelvin // q = carga elétrica do elétron // i2 = corrente de saturação do diodo // k = 1,38 x 10^-23  
+    
+    vr = r*i                                # tensão no resistor = resistencia x tensão
     f = vr - vd - i*r                    # i = corrente no circuito // r = resistência no resistor // k = constante de Boltzmann // 
+
+    print(f)
 
 
 
